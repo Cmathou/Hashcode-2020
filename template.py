eb26997d0a8f6dcb8b0dfbc9248d7cc2abb8e495
Bapt-GR <48047139+Bapt-GR@users.noreply.github.com>
2020-02-20 19:26:15 +0100

    Update template.py

--- template.py
+++ template.py
@@ -5,8 +5,21 @@ import math
 data = open("input.txt", "r")
 lines = data.readlines()
 
-for i in lines:
-	#do magic
+#ligne1 bookNumber B, libNumber L, daysNumber D
+#ligne2 BooksScore * B
+#rest: L * 2lignes
+#	l1: numberOfBooks N, signupTime T, booksPerDay M
+#	l2: BookID * N
+
+
+bookNumber, libNumber, daysNumber = int(i) for i in lines[0]
+BookScores = [int(i) for i in lines[1]]
+
+
+for i in lines[2:]:
+
+
+
 	theMagicReturn = ""
 
 
@@ -14,7 +27,10 @@ for i in lines:
 testFile = open("output.txt", "w")
 
 testFile.write(theMagicReturn)
-
+#ligne1: numberOfLib A, 
+#rest: libDescription * A
+#	l1: libID Y, numberOfBooksScaned K
+#	l2: books (in the order they are sent)
 
 
 testFile.close()
