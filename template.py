987ad084f9b23f9a4cd4ccc47d487bc060a64fc9
Bapt-GR <48047139+Bapt-GR@users.noreply.github.com>
2020-02-20 18:34:27 +0100

    Add files via upload

---
+++ template.py
@@ -0,0 +1,21 @@
+import random
+import numpy
+import math
+
+data = open("input.txt", "r")
+lines = data.readlines()
+
+for i in lines:
+	#do magic
+	theMagicReturn = ""
+
+
+
+testFile = open("output.txt", "w")
+
+testFile.write(theMagicReturn)
+
+
+
+testFile.close()
+data.close()
