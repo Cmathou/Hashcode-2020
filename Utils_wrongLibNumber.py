b6dbd4a2c76dd98d42554b2b2e23e1b891f2ecf2
Bapt-GR <48047139+Bapt-GR@users.noreply.github.com>
2020-02-21 16:12:06 +0100

    Better score but wrong lib number output

---
+++ Utils_wrongLibNumber.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 20:00:07 2020
+
+"""
+
+import numpy as np
+
+def sortBooks(Scores, listBooks):
+	listScores = []
+	for i in range(len(listBooks)):
+		listScores.append(Scores[i])
+	ponderee = [x for _,x in sorted(zip(listScores, listBooks))]
+
+	return ponderee
+
+def CalcLibScore(listBooks, listDays, listNbBooks, Scores, libNumber, jourPasse, jourTot):
+    
+    LibScore = []
+    BooksToSend = []
+    
+    for lib in range(libNumber):
+        #Sort the books by score (the best at first)
+        sortedBooks = sortBooks(Scores, listBooks[lib])
+        slicedList = sortedBooks[:min(len(sortedBooks), (jourTot-jourPasse-listDays[lib]) * listNbBooks[lib])]
+        BooksToSend.append(slicedList)
+        
+        #Calcul du score de la librairy compte tenu de ses meilleurs bouquins et jours
+        LibScore.append(0)
+        
+        for book in slicedList :
+            LibScore[lib] += Scores[book]
+        
+        LibScore[lib] /= listDays[lib]
+    
+    return LibScore, BooksToSend
+
+
+def ChooseBestLib(LibScore):
+    
+    return np.argmax(LibScore)
+
+def DelSuppr(listBooks, BookToDel):
+    
+    for book in BookToDel :
+        for lib in range(len(listBooks)) :
+            if book in listBooks[lib]:
+                listBooks[lib].remove(book)
+                
+    return listBooks
+                
+
+
+
+
+
+#Order :
+"""
+CalcLibScore
+Choose best score
+Envoir Books
+Del books dans toutes les list books
+
+recommence
+
+
+"""
+        
+        
---
+++ template_wrongLibNumber.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+import random
+import math
+from Utils import *
+
+
+inFile = ["a_example.txt", "b_read_on.txt", "c_incunabula.txt", "d_tough_choices.txt", "e_so_many_books.txt", "f_libraries_of_the_world.txt"]
+outFile = ["a_out.txt", "b_out.txt", "c_out.txt", "d_out.txt", "e_out.txt", "f_out.txt"]
+
+continuous = True
+
+#ligne1 bookNumber B, libNumber L, daysNumber D
+#ligne2 BooksScore * B
+#rest: L * 2lignes
+#	l1: numberOfBooks N, signupTime T, booksPerDay M
+#	l2: BookID * N
+def lecture(fileNbr):
+	data = open("problem/" + inFile[fileNbr], "r")
+	lines = data.readlines()
+	data.close()
+
+
+	[bookNumber, libNumber, daysNumber] = [int(i) for i in lines[0].split()]
+	BookScores = [int(i) for i in lines[1].split()]
+
+	libList = []
+	nbJoursSignupList = []
+	BooksPerDayList = []
+
+	l1 = True
+	for i in lines[2:]:
+		if l1:
+			l1 = False
+			[nBooks, signupTime, booksPerDay] = [int(j) for j in i.split()]
+			BooksPerDayList.append(booksPerDay)
+			nbJoursSignupList.append(signupTime)
+		else:
+			l1 = True
+			libList.append([int(j) for j in i.split()])
+
+	return bookNumber, libNumber, daysNumber, libList, nbJoursSignupList, BooksPerDayList, BookScores
+
+
+
+
+def main(fileNbr):
+	nbJour = 0
+	LibSign = []
+	BookSign = []
+	libDone = 0
+	deletedBooks = 0
+	bookNumber, libNumber, daysNumber, libList, nbJoursSignupList, BooksPerDayList, BookScores = lecture(fileNbr)
+	percent = 0
+	if (continuous):
+		file = open(outFile[fileNbr], "w")
+		file.write(str(libNumber) + "\n")
+	while nbJour < daysNumber and libDone < libNumber and deletedBooks < bookNumber:
+		libDone += 1
+		LibScore, BookToSend = CalcLibScore(libList, nbJoursSignupList, BooksPerDayList, BookScores, libNumber, nbJour, daysNumber )
+		indexBest = ChooseBestLib(LibScore)
+
+		libList[indexBest] = []
+
+		if (continuous):
+			continiousSave(file, indexBest, BookToSend[indexBest])
+		else:
+			LibSign.append(indexBest)
+			BookSign.append(BookToSend[indexBest])
+
+		libList = DelSuppr(libList, BookToSend[indexBest])
+		deletedBooks += len(BookToSend[indexBest])
+
+		newPercent = (100*nbJour)/daysNumber
+		if newPercent > percent:
+			percent = newPercent
+			print(newPercent)
+
+
+
+		nbJour += nbJoursSignupList[indexBest]
+	if (continuous):
+		file.close()
+	else:
+		save(fileNbr, LibSign, BookSign)
+
+
+#ligne1: numberOfLib A, 
+#rest: libDescription * A
+#	l1: libID Y, numberOfBooksScaned K
+#	l2: books (in the order they are sent)
+def save(fileNbr, libSign, libBook):
+	file = open(outFile[fileNbr], "w")
+	file.write(str(len(libSign)) + "\n")
+	for i in range(len(libSign)):
+		file.write(arrayToOut([libSign[i], len(libBook[i])]))
+		file.write(arrayToOut(libBook[i]))
+	file.close()
+
+def continiousSave(file, libSign, libBook):
+	file.write(arrayToOut([libSign, np.size(libBook)]))
+	file.write(numpyToOut(libBook))
+
+def arrayToOut(array):
+	ret = ""
+	if array != []:
+		for i in range(len(array) - 1):
+			ret += str(array[i]) + " "
+		if (type(array) == int):
+			ret += str(array) + "\n"
+		else:
+			ret += str(array[-1]) + "\n"
+	return ret
+
+def numpyToOut(array):
+	ret = ""
+	if array != []:
+		for i in range(np.size(array) - 1):
+			ret += str(array[i]) + " "
+		if (np.issubdtype(type(array), np.integer)):
+			ret += str(array) + "\n"
+		else:
+			ret += str(array[-1]) + "\n"
+	return ret
+
+if (__name__ == "__main__"):
+	for i in range(len(inFile)):
+		print "doing", inFile[i]
+		main(i)
+		print inFile[i], "done"
